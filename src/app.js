a955c27c35d94baf5298199bd5738978e9fd0174
seriojaa <kaliashvilisergi@gmail.com>
2025-06-13 16:23:29 +0400

    connect with mognoose and create  model for icecream

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-})
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
