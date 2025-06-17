1a184d435d6611ffefedd80e8c785b153217874d
seriojaa <kaliashvilisergi@gmail.com>
2025-06-17 22:03:00 +0400

    install cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoutes from "./routes/userRoutes.js";
-
+import cors from "cors";
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 
